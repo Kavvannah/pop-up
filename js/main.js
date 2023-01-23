749661791603ad0331690e305518aaef01ad56c1
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2023-01-22 22:30:30 -0800

    :heavy_plus_sign:exit event listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
+const exit = document.querySelector('.close');
 
 button.addEventListener('click', () => {
-    popup.style.display = 'block'
+    popup.style.display = 'block';
 });
+
+exit.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
